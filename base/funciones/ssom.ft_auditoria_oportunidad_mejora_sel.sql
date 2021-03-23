2b9b14502096be843eaed10d69fb3cf30f5f6d69
BaroHyo <boramm@outlook.es>
2021-03-23 14:30:53 -0400

    #SSOM-11 :  Programacion Auditoria

--- base/funciones/ssom.ft_auditoria_oportunidad_mejora_sel.sql
+++ base/funciones/ssom.ft_auditoria_oportunidad_mejora_sel.sql
@@ -1,4 +1,4 @@
-create function ft_auditoria_oportunidad_mejora_sel(p_administrador integer, p_id_usuario integer, p_tabla character varying, p_transaccion character varying) returns character varying
+create or replace function soom.ft_auditoria_oportunidad_mejora_sel(p_administrador integer, p_id_usuario integer, p_tabla character varying, p_transaccion character varying) returns character varying
     language plpgsql
 as
 $$
