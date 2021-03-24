043373e9c1521b6b8b49d4f096e7935d7dedd25c
BaroHyo <boramm@outlook.es>
2021-03-24 10:56:51 -0400

    #SSOM-11 :  Programacion Auditoria

--- base/dependencies000001.sql
+++ base/dependencies000001.sql
@@ -1,7 +1,4 @@
 /***********************************I-DEP-MMV-SSOM-6-4/9/2020*****************************************/
-----------------------------------
---COPY LINES TO dependencies.sql FILE
----------------------------------
 select pxp.f_insert_testructura_gui ('SSOM', 'SISTEMA');
 select pxp.f_insert_testructura_gui ('nor', 'cnor');
 select pxp.f_delete_testructura_gui ('cges', 'SSOM');
@@ -43,22 +40,60 @@ select pxp.f_insert_testructura_gui ('PAY', 'RPTAUD');
 select pxp.f_delete_testructura_gui ('SAS', 'SER');
 select pxp.f_delete_testructura_gui ('VBAUD', 'AUDIN');
 select pxp.f_insert_testructura_gui ('AUD', 'SSOM');
-select pxp.f_insert_testructura_gui ('PAUD', 'AUD');
-select pxp.f_insert_testructura_gui ('PAUDI', 'AUD');
-select pxp.f_insert_testructura_gui ('INFEA', 'AUD');
+select pxp.f_delete_testructura_gui ('PAUD', 'AUD');
+select pxp.f_delete_testructura_gui ('PAUDI', 'AUD');
+select pxp.f_delete_testructura_gui ('INFEA', 'AUD');
 select pxp.f_insert_testructura_gui ('RPTAUD', 'SSOM');
 select pxp.f_insert_testructura_gui ('ODM', 'SSOM');
 select pxp.f_delete_testructura_gui ('SGUI', 'RPTAUD');
 select pxp.f_insert_testructura_gui ('SGUI', 'SSOM');
-select pxp.f_insert_testructura_gui ('NCP', 'AUD');
-select pxp.f_insert_testructura_gui ('ION', 'SGUI');
-select pxp.f_insert_testructura_gui ('OMP', 'ODM');
+select pxp.f_delete_testructura_gui ('NCP', 'AUD');
+select pxp.f_delete_testructura_gui ('ION', 'SGUI');
+select pxp.f_delete_testructura_gui ('OMP', 'ODM');
 select pxp.f_insert_testructura_gui ('ODMS', 'ODM');
 select pxp.f_insert_testructura_gui ('POMD', 'ODM');
-select pxp.f_insert_testructura_gui ('NSC', 'SGUI');
-select pxp.f_insert_testructura_gui ('APD', 'SGUI');
-select pxp.f_insert_testructura_gui ('AID', 'SGUI');
+select pxp.f_delete_testructura_gui ('NSC', 'SGUI');
+select pxp.f_delete_testructura_gui ('APD', 'SGUI');
+select pxp.f_delete_testructura_gui ('AID', 'SGUI');
+select pxp.f_delete_testructura_gui ('AUDI', 'SGUI');
+select pxp.f_insert_testructura_gui ('ER', 'PDAU');
+select pxp.f_delete_testructura_gui ('OMP', 'AUD');
+select pxp.f_delete_testructura_gui ('OMP', 'ODM');
+select pxp.f_delete_testructura_gui ('ION', 'AUD');
+select pxp.f_delete_testructura_gui ('NCA', 'AUD');
+select pxp.f_delete_testructura_gui ('PAS', 'AUD');
+select pxp.f_delete_testructura_gui ('ICS', 'AUD');
+select pxp.f_delete_testructura_gui ('NCD', 'SGUI');
+select pxp.f_delete_testructura_gui ('AIP', 'AUD');
+select pxp.f_delete_testructura_gui ('APP', 'AUD');
+select pxp.f_delete_testructura_gui ('APA', 'AUD');
+select pxp.f_delete_testructura_gui ('ARS', 'AUD');
+select pxp.f_delete_testructura_gui ('NCP', 'SSOM');
+select pxp.f_insert_testructura_gui ('PAS', 'SSOM');
+select pxp.f_insert_testructura_gui ('ARS', 'SSOM');
+select pxp.f_delete_testructura_gui ('AIP', 'SSOM');
+select pxp.f_delete_testructura_gui ('ICS', 'SSOM');
+select pxp.f_delete_testructura_gui ('APA', 'SSOM');
+select pxp.f_insert_testructura_gui ('APP', 'SSOM');
+select pxp.f_delete_testructura_gui ('OMP', 'SSOM');
+select pxp.f_delete_testructura_gui ('NCA', 'SSOM');
+select pxp.f_delete_testructura_gui ('ION', 'SSOM');
+select pxp.f_delete_testructura_gui ('INFEA', 'SSOM');
+select pxp.f_delete_testructura_gui ('PAUDI', 'SSOM');
+select pxp.f_delete_testructura_gui ('PAUD', 'SSOM');
+select pxp.f_insert_testructura_gui ('PAUD', 'AUD');
+select pxp.f_insert_testructura_gui ('PAUDI', 'AUD');
+select pxp.f_insert_testructura_gui ('INFEA', 'AUD');
+select pxp.f_insert_testructura_gui ('OMP', 'ODM');
+select pxp.f_insert_testructura_gui ('PRL', 'SSOM');
+select pxp.f_insert_testructura_gui ('ION', 'PRL');
+select pxp.f_insert_testructura_gui ('NCP', 'PRL');
+select pxp.f_insert_testructura_gui ('NCA', 'PRL');
+select pxp.f_insert_testructura_gui ('AIP', 'PRL');
+select pxp.f_insert_testructura_gui ('ICS', 'PRL');
+select pxp.f_insert_testructura_gui ('APA', 'PRL');
 /***********************************F-DEP-MMV-SSOM-6-4/9/2020*****************************************/
+
 /***********************************I-DEP-MMV-SSOM-6-13/9/2020*****************************************/
 select pxp.f_insert_testructura_gui ('AUDI', 'SGUI');
 /***********************************F-DEP-MMV-SSOM-6-13/8/2020*****************************************/
