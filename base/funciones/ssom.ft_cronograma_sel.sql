a5582b9b51cf8c07ed62349014e0198334224a2d
BaroHyo <boramm@outlook.es>
2021-03-24 20:54:49 -0400

    #SSOM-12 :  Planificación Auditoria

--- base/funciones/ssom.ft_cronograma_sel.sql
+++ base/funciones/ssom.ft_cronograma_sel.sql
@@ -2,7 +2,7 @@ create or replace function ssom.ft_cronograma_sel(p_administrador integer, p_id_
     language plpgsql
 as
 $$
-    /**************************************************************************
+/**************************************************************************
    SISTEMA:		Seguimiento de Oportunidades de Mejora
    FUNCION: 		ssom.ft_cronograma_sel
    DESCRIPCION:   Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'ssom.tcronograma'
@@ -18,10 +18,10 @@ $$
 
 DECLARE
 
-    v_consulta    		varchar;
-    v_parametros  		record;
-    v_nombre_funcion   	text;
-    v_resp				varchar;
+    v_consulta       varchar;
+    v_parametros     record;
+    v_nombre_funcion text;
+    v_resp           varchar;
 
 BEGIN
 
@@ -35,43 +35,44 @@ BEGIN
      #FECHA:		12-12-2019 15:50:53
     ***********************************/
 
-    if(p_transaccion='SSOM_CRONOG_SEL')then
+    if (p_transaccion = 'SSOM_CRONOG_SEL') then
 
         begin
             --Sentencia de la consulta
-            v_consulta:='select
-						cronog.id_cronograma,
-                        cronog.nueva_actividad,
-						cronog.obs_actividad,
-						cronog.estado_reg,
-						cronog.hora_ini_activ,
-						cronog.fecha_ini_activ,
-						cronog.fecha_fin_activ,
-						cronog.id_actividad,
-						cronog.hora_fin_activ,
-						cronog.id_aom,
-						cronog.fecha_reg,
-						cronog.usuario_ai,
-						cronog.id_usuario_reg,
-						cronog.id_usuario_ai,
-						cronog.id_usuario_mod,
-						cronog.fecha_mod,
-						usu1.cuenta as usr_reg,
-						usu2.cuenta as usr_mod,
-                        atv.actividad,
-                         (select pxp.list(initcap (fu.desc_funcionario1))
-                        from ssom.tcronograma_equipo_responsable cc
-                        inner join orga.vfuncionario fu on fu.id_funcionario = cc.id_funcionario
-                        where cc.id_cronograma = cronog.id_cronograma) as lista_funcionario
-						from ssom.tcronograma cronog
-						inner join segu.tusuario usu1 on usu1.id_usuario = cronog.id_usuario_reg
-						left join segu.tusuario usu2 on usu2.id_usuario = cronog.id_usuario_mod
-                        join ssom.tactividad atv on cronog.id_actividad = atv.id_actividad
-				        where  ';
+            v_consulta := 'select  cronog.id_cronograma,
+                                   cronog.nueva_actividad,
+                                   cronog.obs_actividad,
+                                   cronog.estado_reg,
+                                   cronog.hora_ini_activ,
+                                   cronog.fecha_ini_activ,
+                                   cronog.fecha_fin_activ,
+                                   cronog.id_actividad,
+                                   cronog.hora_fin_activ,
+                                   cronog.id_aom,
+                                   cronog.fecha_reg,
+                                   cronog.usuario_ai,
+                                   cronog.id_usuario_reg,
+                                   cronog.id_usuario_ai,
+                                   cronog.id_usuario_mod,
+                                   cronog.fecha_mod,
+                                   usu1.cuenta                                     as usr_reg,
+                                   usu2.cuenta                                     as usr_mod,
+                                   atv.actividad,
+                                   (select pxp.list(initcap(coalesce(fu.desc_funcionario1,ee.exp_tec_externo)))
+                                    from ssom.tcronograma_equipo_responsable cc
+                                             inner join ssom.tequipo_responsable ee on ee.id_equipo_responsable = cc.id_equipo_responsable
+                                             left join orga.vfuncionario fu on fu.id_funcionario = ee.id_funcionario
+                                    where cc.id_cronograma = cronog.id_cronograma) as lista_funcionario
+                            from ssom.tcronograma cronog
+                                     inner join segu.tusuario usu1 on usu1.id_usuario = cronog.id_usuario_reg
+                                     left join segu.tusuario usu2 on usu2.id_usuario = cronog.id_usuario_mod
+                                     join ssom.tactividad atv on cronog.id_actividad = atv.id_actividad
+				            where  ';
 
             --Definicion de la respuesta
-            v_consulta:=v_consulta||v_parametros.filtro;
-            v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
+            v_consulta := v_consulta || v_parametros.filtro;
+            v_consulta := v_consulta || ' order by ' || v_parametros.ordenacion || ' ' || v_parametros.dir_ordenacion ||
+                          ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
 
             --Devuelve la respuesta
             return v_consulta;
@@ -85,11 +86,11 @@ BEGIN
      #FECHA:		12-12-2019 15:50:53
     ***********************************/
 
-    elsif(p_transaccion='SSOM_CRONOG_CONT')then
+    elsif (p_transaccion = 'SSOM_CRONOG_CONT') then
 
         begin
             --Sentencia de la consulta de conteo de registros
-            v_consulta:='select count(id_cronograma)
+            v_consulta := 'select count(id_cronograma)
 					    from ssom.tcronograma cronog
 					    inner join segu.tusuario usu1 on usu1.id_usuario = cronog.id_usuario_reg
 						left join segu.tusuario usu2 on usu2.id_usuario = cronog.id_usuario_mod
@@ -97,7 +98,7 @@ BEGIN
 					    where ';
 
             --Definicion de la respuesta
-            v_consulta:=v_consulta||v_parametros.filtro;
+            v_consulta := v_consulta || v_parametros.filtro;
 
             --Devuelve la respuesta
             return v_consulta;
@@ -113,10 +114,10 @@ BEGIN
 EXCEPTION
 
     WHEN OTHERS THEN
-        v_resp='';
-        v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
-        v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
-        v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
+        v_resp = '';
+        v_resp = pxp.f_agrega_clave(v_resp, 'mensaje', SQLERRM);
+        v_resp = pxp.f_agrega_clave(v_resp, 'codigo_error', SQLSTATE);
+        v_resp = pxp.f_agrega_clave(v_resp, 'procedimientos', v_nombre_funcion);
         raise exception '%',v_resp;
 END;
 $$;
--- base/funciones/ssom.ft_destinatario_ime.sql
+++ base/funciones/ssom.ft_destinatario_ime.sql
@@ -1,202 +1,266 @@
-CREATE OR REPLACE FUNCTION ssom.ft_destinatario_ime (
-  p_administrador integer,
-  p_id_usuario integer,
-  p_tabla varchar,
-  p_transaccion varchar
-)
-RETURNS varchar AS
-$body$
-	/**************************************************************************
-   SISTEMA:		Sistema de Seguimiento a Oportunidades de Mejora
-   FUNCION: 		ssom.ft_destinatario_ime
-   DESCRIPCION:   Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'ssom.tdestinatario'
-   AUTOR: 		 (max.camacho)
-   FECHA:	        10-09-2019 23:09:14
+create or replace function ssom.ft_cronograma_ime(p_administrador integer, p_id_usuario integer, p_tabla character varying,
+                                  p_transaccion character varying) returns character varying
+    language plpgsql
+as
+$$
+/**************************************************************************
+   SISTEMA:		Seguimiento de Oportunidades de Mejora
+   FUNCION: 		ssom.ft_cronograma_ime
+   DESCRIPCION:   Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'ssom.tcronograma'
+   AUTOR: 		MMV
+   FECHA:	        12-12-2019 15:50:53
    COMENTARIOS:
   ***************************************************************************
    HISTORIAL DE MODIFICACIONES:
   #ISSUE				FECHA				AUTOR				DESCRIPCION
-   #0				10-09-2019 23:09:14								Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'ssom.tdestinatario'
+   #0				12-12-2019 15:50:53								Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'ssom.tcronograma'
    #4				04-08-2029 15:51:56		 MMV				    Refactorizacion Planificacion
    ***************************************************************************/
 
 DECLARE
 
-	v_nro_requerimiento    	integer;
-	v_parametros           	record;
-	v_id_requerimiento     	integer;
-	v_resp		            varchar;
-	v_nombre_funcion        text;
-	v_mensaje_error         text;
-	v_id_destinatario_aom	integer;
-    v_revizar				varchar;
+    v_parametros            record;
+    v_resp                  varchar;
+    v_nombre_funcion        text;
+    v_id_cronograma         integer;
+    v_id_funcionario        integer;
+    v_id_equipo_responsable integer;
+
 
 BEGIN
 
-	v_nombre_funcion = 'ssom.ft_destinatario_ime';
-	v_parametros = pxp.f_get_record(p_tabla);
-
-	/*********************************
- 	#TRANSACCION:  'SSOM_DEST_INS'
- 	#DESCRIPCION:	Insercion de registros
- 	#AUTOR:		max.camacho
- 	#FECHA:		10-09-2019 23:09:14
-	***********************************/
-
-	if(p_transaccion='SSOM_DEST_INS')then
-
-		begin
-			--Sentencia de la insercion
-			insert into ssom.tdestinatario(
-			--	id_parametro,
-				id_aom,
-				id_funcionario,
-				estado_reg,
-				id_usuario_ai,
-				fecha_reg,
-				usuario_ai,
-				id_usuario_reg,
-				fecha_mod,
-				id_usuario_mod
-                ) values(
-                 -- v_parametros.id_parametro,
-                  v_parametros.id_aom,
-                  v_parametros.id_funcionario,
-                  'activo',
-                  v_parametros._id_usuario_ai,
-                  now(),
-                  v_parametros._nombre_usuario_ai,
-                  p_id_usuario,
-                  null,
-                  null
-         		  )RETURNING id_destinatario_aom into v_id_destinatario_aom;
-
-          --Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Destinatario almacenado(a) con exito (id_destinatario_aom'||v_id_destinatario_aom||')');
-			v_resp = pxp.f_agrega_clave(v_resp,'id_destinatario_aom',v_id_destinatario_aom::varchar);
-
-			--Devuelve la respuesta
-			return v_resp;
-
-		end;
-
-		/*********************************
-     #TRANSACCION:  'SSOM_DEST_MOD'
-     #DESCRIPCION:	Modificacion de registros
-     #AUTOR:		max.camacho
-     #FECHA:		10-09-2019 23:09:14
-    ***********************************/
-
-	elsif(p_transaccion='SSOM_DEST_MOD')then
-
-		begin
-			--Sentencia de la modificacion
-			update ssom.tdestinatario set
-             -- id_parametro = v_parametros.id_parametro,
-              id_aom = v_parametros.id_aom,
-              id_funcionario = v_parametros.id_funcionario,
-              fecha_mod = now(),
-              id_usuario_mod = p_id_usuario,
-              id_usuario_ai = v_parametros._id_usuario_ai,
-              usuario_ai = v_parametros._nombre_usuario_ai
-			where id_destinatario_aom=v_parametros.id_destinatario_aom;
-
-			--Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Destinatario modificado(a)');
-			v_resp = pxp.f_agrega_clave(v_resp,'id_destinatario_aom',v_parametros.id_destinatario_aom::varchar);
-
-			--Devuelve la respuesta
-			return v_resp;
-
-		end;
-
-		/*********************************
-     #TRANSACCION:  'SSOM_DEST_ELI'
-     #DESCRIPCION:	Eliminacion de registros
-     #AUTOR:		max.camacho
-     #FECHA:		10-09-2019 23:09:14
-    ***********************************/
-
-	elsif(p_transaccion='SSOM_DEST_ELI')then
-
-		begin
-			--Sentencia de la eliminacion
-			delete from ssom.tdestinatario
-			where id_destinatario_aom=v_parametros.id_destinatario_aom;
-
-			--Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Destinatario eliminado(a)');
-			v_resp = pxp.f_agrega_clave(v_resp,'id_destinatario_aom',v_parametros.id_destinatario_aom::varchar);
-
-			--Devuelve la respuesta
-			return v_resp;
-
-		end;
+    v_nombre_funcion = 'ssom.ft_cronograma_ime';
+    v_parametros = pxp.f_get_record(p_tabla);
 
     /*********************************
-     #TRANSACCION:   'SSOM_DCH_INS'
-     #DESCRIPCION:	 check
-     #AUTOR:		 MMV
-     #FECHA:		 29/07/2020
+     #TRANSACCION:  'SSOM_CRONOG_INS'
+     #DESCRIPCION:	Insercion de registros
+     #AUTOR:		MMV
+     #FECHA:		12-12-2019 15:50:53
     ***********************************/
 
-	elsif(p_transaccion='SSOM_DCH_INS')then
+    if (p_transaccion = 'SSOM_CRONOG_INS') then
 
-		begin
-			--Sentencia de la modificacion
+        begin
 
-            select de.incluir_informe into v_revizar
-            from ssom.tdestinatario de
-            where de.id_destinatario_aom = v_parametros.id_destinatario_aom;
+            --Sentencia de la insercion
 
-            if (v_revizar = 'si') then
+            insert into ssom.tcronograma(nueva_actividad,
+                                         estado_reg,
+                                         hora_ini_activ,
+                                         fecha_ini_activ,
+                                         fecha_fin_activ,
+                                         id_actividad,
+                                         hora_fin_activ,
+                                         id_aom,
+                                         fecha_reg,
+                                         usuario_ai,
+                                         id_usuario_reg,
+                                         id_usuario_ai,
+                                         id_usuario_mod,
+                                         fecha_mod)
+            values (v_parametros.nueva_actividad,
+                    'activo',
+                    v_parametros.hora_ini_activ,
+                    v_parametros.fecha_ini_activ,
+                    v_parametros.fecha_fin_activ,
+                    v_parametros.id_actividad,
+                    v_parametros.hora_fin_activ,
+                    v_parametros.id_aom,
+                    now(),
+                    v_parametros._nombre_usuario_ai,
+                    p_id_usuario,
+                    v_parametros._id_usuario_ai,
+                    null,
+                    null)
+            RETURNING id_cronograma into v_id_cronograma;
 
-                update ssom.tdestinatario set
-                incluir_informe = 'no'
-                where id_destinatario_aom = v_parametros.id_destinatario_aom;
+            --Definicion de la respuesta
+            v_resp = pxp.f_agrega_clave(v_resp, 'mensaje',
+                                        'Cronograma almacenado(a) con exito (id_cronograma' || v_id_cronograma || ')');
+            v_resp = pxp.f_agrega_clave(v_resp, 'id_cronograma', v_id_cronograma::varchar);
+
+            --Devuelve la respuesta
+            return v_resp;
+
+        end;
+
+        /*********************************
+     #TRANSACCION:  'SSOM_CRONOG_MOD'
+     #DESCRIPCION:	Modificacion de registros
+     #AUTOR:		MMV
+     #FECHA:		12-12-2019 15:50:53
+    ***********************************/
+
+    elsif (p_transaccion = 'SSOM_CRONOG_MOD') then
+
+        begin
+            --Sentencia de la modificacion
+
+
+            update ssom.tcronograma
+            set nueva_actividad = v_parametros.nueva_actividad,
+                hora_ini_activ  = v_parametros.hora_ini_activ,
+                fecha_ini_activ = v_parametros.fecha_ini_activ,
+                fecha_fin_activ = v_parametros.fecha_fin_activ,
+                id_actividad    = v_parametros.id_actividad,
+                hora_fin_activ  = v_parametros.hora_fin_activ,
+                id_aom          = v_parametros.id_aom,
+                id_usuario_mod  = p_id_usuario,
+                fecha_mod       = now(),
+                id_usuario_ai   = v_parametros._id_usuario_ai,
+                usuario_ai      = v_parametros._nombre_usuario_ai
+            where id_cronograma = v_parametros.id_cronograma;
+
+            --Definicion de la respuesta
+            v_resp = pxp.f_agrega_clave(v_resp, 'mensaje', 'Cronograma modificado(a)');
+            v_resp = pxp.f_agrega_clave(v_resp, 'id_cronograma', v_parametros.id_cronograma::varchar);
+
+            --Devuelve la respuesta
+            return v_resp;
+
+        end;
+
+        /*********************************
+     #TRANSACCION:  'SSOM_CRONOG_ELI'
+     #DESCRIPCION:	Eliminacion de registros
+     #AUTOR:		MMV
+     #FECHA:		12-12-2019 15:50:53
+    ***********************************/
+
+    elsif (p_transaccion = 'SSOM_CRONOG_ELI') then
+
+        begin
+            --Sentencia de la eliminacion
+            delete
+            from ssom.tcronograma
+            where id_cronograma = v_parametros.id_cronograma;
+
+            --Definicion de la respuesta
+            v_resp = pxp.f_agrega_clave(v_resp, 'mensaje', 'Cronograma eliminado(a)');
+            v_resp = pxp.f_agrega_clave(v_resp, 'id_cronograma', v_parametros.id_cronograma::varchar);
+
+            --Devuelve la respuesta
+            return v_resp;
+
+        end;
+
+
+        /*********************************
+         #TRANSACCION:  'SSOM_CROIN_INS'
+         #DESCRIPCION:	Eliminacion de registros
+         #AUTOR:		MMV
+         #FECHA:		12-12-2019 15:50:53
+        ***********************************/
+
+    elsif (p_transaccion = 'SSOM_CROIN_INS') then
+
+        begin
+            --Sentencia de la eliminacion
+
+            if (v_parametros.id_cronograma is not null) then
+
+                delete
+                from ssom.tcronograma_equipo_responsable cr
+                where cr.id_cronograma = v_parametros.id_cronograma;
+
+                delete
+                from ssom.tcronograma c
+                where c.id_cronograma = v_parametros.id_cronograma;
+
 
             end if;
 
-            if (v_revizar = 'no') then
 
-                update ssom.tdestinatario set
-                incluir_informe = 'si'
-                where id_destinatario_aom = v_parametros.id_destinatario_aom;
+            insert into ssom.tcronograma(estado_reg,
+                                         hora_ini_activ,
+                                         fecha_ini_activ,
+                                         fecha_fin_activ,
+                                         id_actividad,
+                                         hora_fin_activ,
+                                         id_aom,
+                                         fecha_reg,
+                                         usuario_ai,
+                                         id_usuario_reg,
+                                         id_usuario_ai,
+                                         id_usuario_mod,
+                                         fecha_mod)
+            values ('activo',
+                    v_parametros.hora_ini_activ,
+                    v_parametros.fecha_ini_activ,
+                    v_parametros.fecha_fin_activ,
+                    v_parametros.id_actividad,
+                    v_parametros.hora_fin_activ,
+                    v_parametros.id_aom,
+                    now(),
+                    v_parametros._nombre_usuario_ai,
+                    p_id_usuario,
+                    v_parametros._id_usuario_ai,
+                    null,
+                    null)
+            RETURNING id_cronograma into v_id_cronograma;
 
-            end if;
+            -- raise exception '%',v_parametros.funcionarios::varchar;
 
-			--Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Destinatario modificado(a)');
-			v_resp = pxp.f_agrega_clave(v_resp,'id_destinatario_aom',v_parametros.id_destinatario_aom::varchar);
+            foreach v_id_equipo_responsable IN array (string_to_array(v_parametros.funcionarios::varchar, ','))
+                loop
 
-			--Devuelve la respuesta
-			return v_resp;
+                    select ar.id_funcionario
+                    into v_id_funcionario
+                    from ssom.tequipo_responsable ar
+                    where ar.id_equipo_responsable = v_id_equipo_responsable;
 
-		end;
 
-	else
+                    insert into ssom.tcronograma_equipo_responsable(estado_reg,
+                                                                    id_funcionario,
+                                                                    id_equipo_responsable,
+                                                                    id_cronograma,
+                                                                    fecha_reg,
+                                                                    usuario_ai,
+                                                                    id_usuario_reg,
+                                                                    id_usuario_ai,
+                                                                    id_usuario_mod,
+                                                                    fecha_mod)
+                    values ('activo',
+                            v_id_funcionario,
+                            v_id_equipo_responsable,
+                            v_id_cronograma,
+                            now(),
+                            v_parametros._nombre_usuario_ai,
+                            p_id_usuario,
+                            v_parametros._id_usuario_ai,
+                            null,
+                            null);
 
-		raise exception 'Transaccion inexistente: %',p_transaccion;
+                end loop;
 
-	end if;
 
-	EXCEPTION
+            --Definicion de la respuesta
+            v_resp = pxp.f_agrega_clave(v_resp, 'mensaje', 'Cronograma eliminado(a)');
+            v_resp = pxp.f_agrega_clave(v_resp, 'id_cronograma', v_parametros.id_aom::varchar);
 
-	WHEN OTHERS THEN
-		v_resp='';
-		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
-		v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
-		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
-		raise exception '%',v_resp;
+            --Devuelve la respuesta
+            return v_resp;
+
+        end;
+
+
+    else
+
+        raise exception 'Transaccion inexistente: %',p_transaccion;
+
+    end if;
+
+EXCEPTION
+
+    WHEN OTHERS THEN
+        v_resp = '';
+        v_resp = pxp.f_agrega_clave(v_resp, 'mensaje', SQLERRM);
+        v_resp = pxp.f_agrega_clave(v_resp, 'codigo_error', SQLSTATE);
+        v_resp = pxp.f_agrega_clave(v_resp, 'procedimientos', v_nombre_funcion);
+        raise exception '%',v_resp;
 
 END;
-$body$
-LANGUAGE 'plpgsql'
-VOLATILE
-CALLED ON NULL INPUT
-SECURITY INVOKER
-PARALLEL UNSAFE
-COST 100;
+$$;
 
-ALTER FUNCTION ssom.ft_destinatario_ime (p_administrador integer, p_id_usuario integer, p_tabla varchar, p_transaccion varchar)
-  OWNER TO postgres;
+
